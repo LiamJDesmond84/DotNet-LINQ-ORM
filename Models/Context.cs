f8143b274d639a91c85d2c7c64c74d94a9a90f05
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-06 18:43:27 -0500

    Adding notes

--- Models/Context.cs
+++ Models/Context.cs
@@ -12,6 +12,8 @@ namespace SportsORM.Models
         protected override void OnConfiguring(DbContextOptionsBuilder options) 
             => options.UseSqlite("Data Source=SportsORM.db");
 
+
+        // DBSet<Leagues/Teams/Players> directly connected to the DataController's static tables
         public DbSet<League> Leagues {get;set;}
         public DbSet<Team> Teams {get;set;}
         public DbSet<Player> Players {get;set;}
