cf3ab2b107b200c2ebf1cf912994fcf0448b2727
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-11 19:34:45 -0500

    Adding more queries

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,9 +41,8 @@ namespace SportsORM.Controllers
             ViewBag.Raptors = _context.Teams.Where(x => x.TeamName.Contains("Raptors")).ToList();
             ViewBag.City = _context.Teams.Where(x => x.Location.Contains("City")).ToList();
             ViewBag.TStart = _context.Teams.Where(x => x.TeamName.StartsWith("T")).ToList();
-            ViewBag.Alpha = _context.Teams.ToList().OrderBy(x => x.TeamName);
-            ViewBag.Dallas = _context.Teams.Where(x => x.Location.Contains("Dallas")).ToList();
-            ViewBag.Dallas = _context.Teams.Where(x => x.Location.Contains("Dallas")).ToList();
+            ViewBag.AlphaLocation = _context.Teams.OrderBy(x => x.Location).ToList();
+            ViewBag.ReverseAlpha = _context.Teams.OrderByDescending(x => x.TeamName).ToList();
             return View();
         }
 
